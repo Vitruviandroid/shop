5d7b68cec87c0b80c87b5bddbc1b0229d47fc0d1
yura <deadpool321214@gmail.com>
2022-07-27 18:46:50 +0300

    push

---
+++ js/form.js
@@ -0,0 +1,29 @@
+
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+    service: "form",
+    auth: {
+        user: 'formposta009@gmail.com',
+        pass: '12345678qwertyui'
+    },
+})
+
+let option = await transporter.sendMail({
+    from: 'formposta009@gmail.com',
+    to: 'deadpool321214@gmail.com',
+    subject: 'Message from Node js',
+    text: 'This message was sent from Node js server.',
+})
+
+transporter.sendMail(option, function (err, info) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log("Sent" + info.response);
+})
+
+document.querySelector("form").addEventListener("click", function (event) {
+    event.preventDefault()
+});
---
+++ js/window.js
@@ -0,0 +1,12 @@
+
+// const body = document.querySelector('.window__container')
+// const windowBtn = document.querySelector('.window__btn')
+// const close__window = document.querySelector('.close__w')
+
+// windowBtn.addEventListener('click', () => {
+//     body.classList.toggle('active__w')
+// })
+// close__window.addEventListener('click', () => {
+//     body.classList.toggle('active__w')
+// })
+
