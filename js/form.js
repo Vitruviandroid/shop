847c39a8cb82261cdf88a6225390b5ca83549857
yura <deadpool321214@gmail.com>
2022-07-27 18:57:23 +0300

    push 3

--- js/form.js
+++ js/form.js
@@ -1,28 +1,28 @@
 
-// const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer')
 
-// const transporter = nodemailer.createTransport({
-//     service: "form",
-//     auth: {
-//         user: 'formposta009@gmail.com',
-//         pass: '12345678qwertyui'
-//     },
-// })
+const transporter = nodemailer.createTransport({
+    service: "form",
+    auth: {
+        user: 'formposta009@gmail.com',
+        pass: '12345678qwertyui'
+    },
+})
 
-// let option = await transporter.sendMail({
-//     from: 'formposta009@gmail.com',
-//     to: 'deadpool321214@gmail.com',
-//     subject: 'Message from Node js',
-//     text: 'This message was sent from Node js server.',
-// })
+let option = await transporter.sendMail({
+    from: 'formposta009@gmail.com',
+    to: 'deadpool321214@gmail.com',
+    subject: 'Message from Node js',
+    text: 'This message was sent from Node js server.',
+})
 
-// transporter.sendMail(option, function (err, info) {
-//     if (err) {
-//         console.log(err);
-//         return
-//     }
-//     console.log("Sent" + info.response);
-// })
+transporter.sendMail(option, function (err, info) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log("Sent" + info.response);
+})
 
 document.querySelector("form").addEventListener("click", function (event) {
     event.preventDefault()
