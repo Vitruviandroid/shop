864bf38e8c44e76dcc4c0b6effe00c362c7d713e
yura <deadpool321214@gmail.com>
2022-07-27 19:18:01 +0300

    push 5

--- js/form.js
+++ js/form.js
@@ -1,28 +1,28 @@
-var nodemailer = require('nodemailer');
+document.querySelector("form").addEventListener("click", function (event) {
+  var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'formposta009@gmail.com',
-    pass: '12345678qwertyui'
-  }
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'formposta009@gmail.com',
+      pass: '12345678qwertyui'
+    }
+  });
+
+  var mailOptions = {
+    from: 'formposta009@gmail.com',
+    to: 'deadpool321214@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+
 });
-
-var mailOptions = {
-  from: 'formposta009@gmail.com',
-  to: 'deadpool321214@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
-
-// document.querySelector("form").addEventListener("click", function (event) {
-//     event.preventDefault()
-// });
